cb14219952e2038f11d0dfcb7f53485443532f7e
Lea <leama5@hotmail.com>
2026-09-09 22:52:19 +0300

    +1 painonappi keskeytys tehty

--- Vko2/src/led.c
+++ Vko2/src/led.c
@@ -2,14 +2,20 @@
 #include <zephyr/sys/printk.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/sys/util.h>
+#include <inttypes.h>
 
-
-//Tavoitteena 1 viikolta täydet pisteet!
+//Tavoitteena 2 viikolta täydet pisteet!
 
 // Led pin configurations
 static const struct gpio_dt_spec red = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
 static const struct gpio_dt_spec green = GPIO_DT_SPEC_GET(DT_ALIAS(led1), gpios);
 static const struct gpio_dt_spec blue = GPIO_DT_SPEC_GET(DT_ALIAS(led2), gpios);
+
+#define BUTTON_0 DT_ALIAS(sw0)
+// #define BUTTON_1 DT_ALIAS(sw1)
+static const struct gpio_dt_spec button_0 = GPIO_DT_SPEC_GET_OR(BUTTON_0, gpios, {0});
+static struct gpio_callback button_0_data;
 
 // Red led thread initialization
 #define STACKSIZE 500
@@ -18,10 +24,22 @@ void red_led_task(void *, void *, void*);
 void yellow_led_task(void *, void *, void *);
 void green_led_task(void *, void *, void *);
 int led_state=0;
+int old_led_state=0;
 
 K_THREAD_DEFINE(red_thread,STACKSIZE,red_led_task,NULL,NULL,NULL,PRIORITY,0,0);
 K_THREAD_DEFINE(yellow_thread,STACKSIZE,yellow_led_task,NULL,NULL,NULL,PRIORITY,0,0);
 K_THREAD_DEFINE(green_thread,STACKSIZE,green_led_task,NULL,NULL,NULL,PRIORITY,0,0);
+
+void button_0_handler(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+	if (led_state != 4){
+		old_led_state=led_state;
+		led_state=4;
+	}
+	else 
+		led_state=old_led_state;
+	printk("Button pressed\n");
+}
 
 // Initialize leds
 int  init_led() {
@@ -57,10 +75,46 @@ int  init_led() {
 	return 0;
 }
 
-// Main program
+
+// Button initialization
+int init_button() {
+
+	int ret;
+	if (!gpio_is_ready_dt(&button_0)) {
+		printk("Error: button 0 is not ready\n");
+		return -1;
+	}
+
+	ret = gpio_pin_configure_dt(&button_0, GPIO_INPUT);
+	if (ret != 0) {
+		printk("Error: failed to configure pin\n");
+		return -1;
+	}
+
+	ret = gpio_pin_interrupt_configure_dt(&button_0, GPIO_INT_EDGE_TO_ACTIVE);
+	if (ret != 0) {
+		printk("Error: failed to configure interrupt on pin\n");
+		return -1;
+	}
+
+	gpio_init_callback(&button_0_data, button_0_handler, BIT(button_0.pin));
+	gpio_add_callback(button_0.port, &button_0_data);
+	printk("Set up button 0 ok\n");
+	
+	return 0;
+}
+
 int main(void)
 {
 	init_led();
+    int ret = init_button();
+	if (ret < 0) {
+		return 0;
+	}
+
+	while (1) {
+		k_msleep(10); // sleep 10ms
+	}
 
 	return 0;
 }
@@ -77,9 +131,11 @@ void red_led_task(void *, void *, void*) {
 			// 2. sleep for 2 seconds
 			k_sleep(K_SECONDS(1));
 			// 3. set led off
+			if (led_state != 4){
 			gpio_pin_set_dt(&red,0);
 			printk("Red off\n");
 			led_state=1;
+			}
 		}
 		k_msleep(100);
 	}
@@ -95,10 +151,13 @@ void yellow_led_task(void *, void *, void*) {
 			printk("Yellow on\n");
 			// 2. sleep for 2 seconds
 			k_sleep(K_SECONDS(1));
+			if (led_state != 4){
 			// 3. set led off
 			gpio_pin_set_dt(&red,0);
+			gpio_pin_set_dt(&green,0);
 			printk("Yellow off\n");
 			led_state=2;
+			}
 		}
 		k_msleep(100);
 	}
@@ -114,11 +173,15 @@ void green_led_task(void *, void *, void*) {
 			printk("Green on\n");
 			// 2. sleep for 2 seconds
 			k_sleep(K_SECONDS(1));
+			if (led_state != 4){
 			// 3. set led off
 			gpio_pin_set_dt(&green,0);
 			printk("Green off\n");
 			led_state = 0;
+			}
 		}
 		k_msleep(100);
 	}
 }
+
+
