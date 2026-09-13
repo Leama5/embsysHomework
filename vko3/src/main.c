b4cca1137ca0f09be574cad63311c090112818e5
Lea <leama5@hotmail.com>
2026-09-13 18:04:05 +0300

    perustehtävät tehty

--- vko3/src/main.c
+++ vko3/src/main.c
@@ -13,19 +13,42 @@
 static const struct gpio_dt_spec red = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
 static const struct gpio_dt_spec green = GPIO_DT_SPEC_GET(DT_ALIAS(led1), gpios);
 static const struct gpio_dt_spec blue = GPIO_DT_SPEC_GET(DT_ALIAS(led2), gpios);
+
+
+#define BUTTON_0 DT_ALIAS(sw0)
+#define BUTTON_1 DT_ALIAS(sw1)
+#define BUTTON_2 DT_ALIAS(sw2)
+#define BUTTON_3 DT_ALIAS(sw3)
+#define BUTTON_4 DT_ALIAS(sw4)
+// #define BUTTON_1 DT_ALIAS(sw1)
+static const struct gpio_dt_spec button_0 = GPIO_DT_SPEC_GET_OR(BUTTON_0, gpios, {0});
+static struct gpio_callback button_0_data;
+static const struct gpio_dt_spec button_1 = GPIO_DT_SPEC_GET_OR(BUTTON_1, gpios, {0});
+static struct gpio_callback button_1_data;
+static const struct gpio_dt_spec button_2 = GPIO_DT_SPEC_GET_OR(BUTTON_2, gpios, {0});
+static struct gpio_callback button_2_data;
+static const struct gpio_dt_spec button_3 = GPIO_DT_SPEC_GET_OR(BUTTON_3, gpios, {0});
+static struct gpio_callback button_3_data;
+static const struct gpio_dt_spec button_4 = GPIO_DT_SPEC_GET_OR(BUTTON_4, gpios, {0});
+static struct gpio_callback button_4_data;
 // Thread initializations
 #define STACKSIZE 500
 #define PRIORITY 5
 
 void red_led_task(void *, void *, void*);
 void yellow_led_task(void *, void *, void *);
+void yellow_flash_task(void *, void *, void *);
 void green_led_task(void *, void *, void *);
-int led_state=0;
-int old_led_state=0;
+
+int button1=0;
+int button2=0;
+int button3=0;
+int button4=0;
 
 
 K_THREAD_DEFINE(red_thread,STACKSIZE,red_led_task,NULL,NULL,NULL,PRIORITY,0,0);
 K_THREAD_DEFINE(yellow_thread,STACKSIZE,yellow_led_task,NULL,NULL,NULL,PRIORITY,0,0);
+K_THREAD_DEFINE(yellow_flash_thread,STACKSIZE,yellow_flash_task,NULL,NULL,NULL,PRIORITY,0,0);
 K_THREAD_DEFINE(green_thread,STACKSIZE,green_led_task,NULL,NULL,NULL,PRIORITY,0,0);
 // UART initialization
 #define UART_DEVICE_NODE DT_CHOSEN(zephyr_shell_uart)
@@ -53,6 +76,39 @@ struct data_t {
 	char msg[20];
 };
 
+void button_0_handler(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+	printk("Button pressed\n");
+	//ei tapahdu mitään tällä hetkellä
+
+}
+void button_1_handler(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+	printk("1 Button pressed\n");
+	//lähetetään signaali red valotaskille
+	k_condvar_broadcast(&red_signal);
+}
+void button_2_handler(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+	printk("2 button pressed \n");
+	k_condvar_broadcast(&yellow_signal);
+
+}
+void button_3_handler(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+	printk("3 Button pressed\n");
+	k_condvar_broadcast(&green_signal);
+}
+void button_4_handler(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+	printk("4 Button pressed\n");
+	if (button4 == 0){
+		button4=1;
+	}
+	else
+		button4=0;
+
+}
 /********************
  * init UART
  */
@@ -61,6 +117,52 @@ int init_uart(void) {
 	if (!device_is_ready(uart_dev)) {
 		return 1;
 	} 
+	return 0;
+}
+
+// Button initialization
+int init_button() {
+	static const struct gpio_dt_spec buttons[] = {
+    button_0,
+    button_1,
+    button_2,
+    button_3,
+    button_4
+	};
+	int ret;
+
+	for (int i=0; i<5; i++){
+		if (!gpio_is_ready_dt(&buttons[i])) {
+			printk("Error: button %d is not ready\n", i);
+			return -1;
+		}
+		ret = gpio_pin_configure_dt(&buttons[i], GPIO_INPUT);
+		if (ret != 0) {
+				printk("Error: failed to configure interrupt for button %d\n", i);
+				return -1;
+		}
+		ret = gpio_pin_interrupt_configure_dt(&buttons[i], GPIO_INT_EDGE_TO_ACTIVE);
+		if (ret != 0) {
+			printk("Error: failed to configure interrupt for button %d\n",i);
+			return -1;
+		}
+	}
+
+	gpio_init_callback(&button_0_data, button_0_handler, BIT(button_0.pin));
+	gpio_add_callback(button_0.port, &button_0_data);
+	printk("Set up button 0 ok\n");
+	gpio_init_callback(&button_1_data, button_1_handler, BIT(button_1.pin));
+	gpio_add_callback(button_1.port, &button_1_data);
+	printk("Set up button 1 ok\n");
+	gpio_init_callback(&button_2_data, button_2_handler, BIT(button_2.pin));
+	gpio_add_callback(button_2.port, &button_2_data);
+	printk("Set up button 2 ok\n");
+	gpio_init_callback(&button_3_data, button_3_handler, BIT(button_3.pin));
+	gpio_add_callback(button_3.port, &button_3_data);
+	printk("Set up button 3 ok\n");
+	gpio_init_callback(&button_4_data, button_4_handler, BIT(button_4.pin));
+	gpio_add_callback(button_4.port, &button_4_data);
+	printk("Set up button 4 ok\n");
 	return 0;
 }
 
@@ -109,7 +211,7 @@ int main(void)
 		return ret;
 	}
 	init_led();
-
+	init_button();
 	return 0;
 }
 
@@ -258,6 +360,22 @@ void green_led_task(void *, void *, void*) {
 		printk("Green off\n");
 
 		k_condvar_broadcast(&release_signal);
+	}
+}
+
+void yellow_flash_task(void *, void *, void *){
+	while (true){
+		if (button4 ==1){
+		gpio_pin_set_dt(&red,1);
+		gpio_pin_set_dt(&green,1);
+		k_sleep(K_SECONDS(1));
+		gpio_pin_set_dt(&red,0);
+		gpio_pin_set_dt(&green,0);
+		k_sleep(K_SECONDS(1));
+		}
+		else{
+			k_msleep(10);
+		}
 	}
 }
 
