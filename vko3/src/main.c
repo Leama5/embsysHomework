5e24a48714a8f204bda7d2206db7becd71ab98ac
Lea <leama5@hotmail.com>
2026-09-13 18:05:20 +0300

    Perustehtävät tehty

--- vko3/src/main.c
+++ vko3/src/main.c
@@ -9,6 +9,9 @@
  * CONFIG_HEAP_MEM_POOL_SIZE=1024
  * to prj.conf
  ****************************/
+
+ //Perustehtävät tehtynä tavoitteena tehdä myös lisää lisäominaisuuksia tulevina viikkoina
+
 // Led pin configurations
 static const struct gpio_dt_spec red = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
 static const struct gpio_dt_spec green = GPIO_DT_SPEC_GET(DT_ALIAS(led1), gpios);
